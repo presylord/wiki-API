308e11d69c8b9ecb72a0429c055334da35c034a0
Presy Lord Kidlat <kidlat123@gmail.com>
2021-11-23 22:32:39 +0800

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const articleSchema = {
     type: String,
     required: true,
   },
-  content: String,
+  article: String,
 };
 
 const Article = mongoose.model("articles", articleSchema);
@@ -22,6 +22,37 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find({}, function (err, result) {
+      if (!err) {
+        res.send(result);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    var title = req.body.title;
+    var article = req.body.article;
+    var newArticle = new Article({
+      title: title,
+      article: article,
+    });
+    newArticle.save();
+    res.send("Article Saved");
+  })
+  .delete(function (req, res) {
+    Article.deleteMany({}, function (err) {
+      if (!err) {
+        res.send("All entries deleted successfully.");
+      }
+    });
+  });
+
+app.route("/get").get(function (req, res) {});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
